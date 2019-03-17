570ecae9bc9c7c37d8d10252a3427b626c1ee999
ikodes Technology <jiitendra.yadav>
2019-03-17 19:24:01 +0530

    Implement Left Sidebar

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -38,9 +38,35 @@ class AppAsset extends AssetBundle
     ];
     public $js = [
         'comman/js/jquery-ui.min.js',
+        'comman/js/jquery.dataTables.min.js',
+        'comman/js/dataTables.bootstrap.min.js',
+        'https://cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js',
+        '//cdn.datatables.net/buttons/1.2.2/js/buttons.flash.min.js',
+        '//cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js',
+        '//cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js',
+        '//cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js',
+        '//cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js',
+        '//cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js', 
+        'comman/js/moment.min.js',
+        'comman/js/daterangepicker.js',
+        'comman/js/jquery.form-validator.min.js',
+        'comman/js/app.min.js',
+        'comman/js/icheck.min.js',
+        'comman/ckeditor/ckeditor.js',
+        'comman/ckeditor/adapters/jquery.js',
+        'comman/plugins/moment/moment.min.js',
+        'comman/plugins/fullcalendar/fullcalendar.min.js',
+        'comman/plugins/fileinput/js/fileinput.min.js',
+        'comman/js/demo.js',
+        'comman/js/custom.js',
+
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
     ];
+
+    public $jsOptions = array(
+        // 'position' => \yii\web\View::POS_HEAD
+    );
 }
